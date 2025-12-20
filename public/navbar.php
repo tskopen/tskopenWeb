954d44a3479b364d407218f7de27aaac2d15e468
tskopen <tskopen@gmail.com>
2025-12-20 01:43:05 -0500

    Update navbar.php

--- public/navbar.php
+++ public/navbar.php
@@ -1,8 +1,7 @@
 <nav>
   <ul>
     <li><a href="index.php">Home</a></li>
-    <li><a href="about.php">About</a></li>
-    <li><a href="projects.php">Projects</a></li>
-    <li><a href="contact.php">Contact</a></li>
+    <li><a href="resume.php">About</a></li>
+    <li><a href="rocketry.php">Projects</a></li>
   </ul>
 </nav>
