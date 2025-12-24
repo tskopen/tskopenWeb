f880ead8168d3d8a1acc560ccbfbd426b2b6c1a0
tskopen <tskopen@gmail.com>
2025-12-24 11:55:05 -0500

    Update config.php

--- public/config.php
+++ public/config.php
@@ -1,4 +1,4 @@
 <?php
-$version = "1.04.10"; 
+$version = "1.04.11"; 
 ?>
-<script src="/js/color-picker.js" defer></script>
+<script src="/js/color-picker.js?v=<?php echo $version; ?>" defer></script>
