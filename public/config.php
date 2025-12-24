5498c5baf37345473aa96826d424164613996038
tskopen <tskopen@gmail.com>
2025-12-24 13:02:15 -0500

    plotly update

--- public/config.php
+++ public/config.php
@@ -1,4 +1,4 @@
 <?php
-$version = "1.04.24"; 
+$version = "1.04.25"; 
 ?>
 <script src="/js/color-picker.js?v=<?php echo $version; ?>" defer></script>
--- public/groundstation.php
+++ public/groundstation.php
@@ -5,26 +5,32 @@
         <link rel="stylesheet" href="styles.css?v=<?php echo $version; ?>">
         <title>GroundStationUI</title>
         <script src="https://cdn.plot.ly/plotly-3.3.0.min.js" charset="utf-8"></script>
+        <script src="/js/color-plotly.js?v=<?php echo $version; ?>" defer></script>
+
     </head>
 
     <?php include "navbar.php"; ?>
 
-    <main class="project">
 
     <body>
-        <header>
-            <h1>Groudnstation display for ARCS</h1>
-            <div class="subtitle">
-                Portable UI for ARCS.
+        <main class="project">
+
+            <header>
+                <h1>Groudnstation display for ARCS</h1>
+                <div class="subtitle">
+                    Portable UI for ARCS.
+                </div>
+            </header>
+            <div>
+                <h1>Introduction</h1>
+                <p>
+                    I put the groundstation on a webserver for ease of transportation and portability. AKA I dont need a drag a spare monitor places.
+                </p>
             </div>
-        </header>
-        <div>
-            <h1>Introduction</h1>
-            <p>
-                I put the groundstation on a webserver for ease of transportation and portability. AKA I dont need a drag a spare monitor places.
-            </p>
-        </div>
-        
-        <div id="barometerchart" style="width:100%;height:400px;"></div>
+            
+            <div id="barometerchart" style="width:100%;height:400px;"></div>
+
+        </main>
     </body>
+
 </html>
