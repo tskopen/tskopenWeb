bcf175d24d0fed609907a47e603c12997b15af52
Thor Kopenkoskey <80185495+tskopen@users.noreply.github.com>
2025-12-26 21:57:26 -0500

    restore website prior to node.js

--- MainSite/public/config.php
+++ MainSite/public/config.php
@@ -1,4 +1,4 @@
 <?php
-$version = "1.05.5"; 
+$version = "1.04.26"; 
 ?>
 <script src="/js/color-picker.js?v=<?php echo $version; ?>" defer></script>
--- MainSite/start.sh
+++ MainSite/start.sh
@@ -6,6 +6,5 @@ rm -rf /var/www/html/tmp/*
 echo "⟳ Starting PHP-FPM..."
 php-fpm &  # Runs in background
 
-
 echo "⟳ Starting Nginx..."
 nginx -g "daemon off;"  # Runs in foreground to keep container alive
