745e3397c5d7db9bbf89cb32da2adaaebe51da27
Thor Kopenkoskey <80185495+tskopen@users.noreply.github.com>
2025-12-26 21:41:12 -0500

    overhaul

---
+++ MainSite/public/config.php
@@ -0,0 +1,4 @@
+<?php
+$version = "1.05.5"; 
+?>
+<script src="/js/color-picker.js?v=<?php echo $version; ?>" defer></script>
---
+++ MainSite/public/navbar.php
@@ -0,0 +1,25 @@
+<?php include 'config.php'; ?>
+<link rel="stylesheet" href="styles.css?v=<?php echo $version; ?>">
+
+<nav>
+  <ul class="nav-menu">
+    <li><a href="index.php">Home</a></li>
+    <li><a href="resume.php">Resume</a></li>
+
+    <li class="dropdown">
+      <a href="#" class="dropbtn">Projects</a>
+      <ul class="dropdown-content">
+        <li><a href="rocketry.php">Rocketry (ARCS)</a></li>
+        <li><a href="groundstation.php">GroundStationUI</a></li>
+
+        <li><a href="fair2025.php">Materials Study</a></li>
+        <li><a href="websiteproject.php">This Website</a></li>
+
+      </ul>
+    </li>
+
+    <li class="pushleft"><a href="settings.php">Settings</a></li>
+  </ul>
+</nav>
+
+
