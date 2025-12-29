975f35eb8b9218c02402db7944c393f188058c39
Thor Kopenkoskey <80185495+tskopen@users.noreply.github.com>
2025-12-29 10:31:24 -0500

    Cleanup index and prepare for presentation

--- MainSite/public/index.php
+++ MainSite/public/index.php
@@ -12,7 +12,12 @@
 
 
     <main>
-    
+           <section>
+            <h2>Who Am I</h2></h2>
+            <p>
+            My name is Thor Kopenkoskey, I am a student at Florida Institute of Technology, and I create engineering projects, rock climb, run, in my free time.
+            </p>
+        </section> 
         <section>
             <h2>What I do</h2></h2>
             <ul>
@@ -20,16 +25,16 @@
                     <strong>Research</strong> — Experimental testing, data analysis, and technical writing.
                 </li>
                 <li>
-                    <strong>Engineering</strong> — Hardware systems, materials testing, and applied design.
+                    <strong>Engineering</strong> — Hardware systems, materials testing, and applied design. See resume and Projects dropdown for more info.
                 </li>
                 <li>
-                    <strong>Leadership</strong> — Team leadership, project management, and technical instruction.
+                    <strong>Leadership</strong> — Team leadership, project management, and technical instruction. See resume for positions filled.
                 </li>
             </ul>
         </section>
     
         <section>
-            <h2>Featured Projects</h2>
+            <h2>Featured Projects, See Projects dropdown on the top of the page.</h2>
     
             <article>
                 <h3>
@@ -38,9 +43,6 @@
                 <p>
                     Experimental analysis of carbon fiber reinforcement in sidewalk concrete
                     under flexural strength testing (ASTM C78–02).
-                </p>
-                <p>
-                    <a href="carbon-fiber-concrete-2025.html">Read project</a>
                 </p>
             </article>
     
--- MainSite/public/navbar.php
+++ MainSite/public/navbar.php
@@ -10,8 +10,6 @@
       <a href="#" class="dropbtn">Projects</a>
       <ul class="dropdown-content">
         <li><a href="rocketry.php">Rocketry (ARCS)</a></li>
-        <li><a href="groundstation.php">GroundStationUI</a></li>
-
         <li><a href="fair2025.php">Materials Study</a></li>
         <li><a href="websiteproject.php">This Website</a></li>
 
