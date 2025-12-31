568cd2cbdabfa0262fd0040daad8afcd73098dc8
tskopen <tskopen@gmail.com>
2025-12-31 14:56:10 -0500

    update

--- MainSite/public/php/navbar.php
+++ MainSite/public/php/navbar.php
@@ -2,20 +2,19 @@
 
 <nav>
   <ul class="nav-menu">
-    <li><a href="index.php">Home</a></li>
-    <li><a href="resume.php">Resume</a></li>
+    <li><a href="/php/index.php">Home</a></li>
+    <li><a href="/php/resume.php">Resume</a></li>
 
     <li class="dropdown">
       <a href="#" class="dropbtn">Projects</a>
       <ul class="dropdown-content">
-        <li><a href="rocketry.php">Rocketry (ARCS)</a></li>
-        <li><a href="fair2025.php">Materials Study</a></li>
-        <li><a href="websiteproject.php">This Website</a></li>
+        <li><a href="/php/rocketry.php">Rocketry (ARCS)</a></li>
+        <li><a href="/php/fair2025.php">Materials Study</a></li>
+        <li><a href="/php/websiteproject.php">This Website</a></li>
 
       </ul>
     </li>
 
-<li class="pushleft"><button id="fancyBtn">Toggle Fancy</button></li>
   </ul>
 </nav>
 
